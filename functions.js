9e24bb2496ad41099f29a960a1663f5503d25034
sophieluo <sophie@groobusiness.com>
2016-10-27 22:43:54 -0700

    solved problem 4

--- functions.js
+++ functions.js
@@ -1,1 +1,43 @@
 // TODO: add your solutions here!
+
+//first function
+function combineWords(word1, word2) {
+    return word1+word2
+}
+
+var result = combineWords('dog', 'house');
+console.log(result);
+
+//second function
+
+function repeatPhrase(phrase, n) {
+
+   for (var i=0;i<n;i++) {
+     console.log(phrase)
+   }
+}
+
+repeatPhrase("Hello", 5);
+
+//third function
+function toTheNthPower(number, power) {
+    counter=1;
+    for (var i=0;i<power;i++) {
+        counter=counter*number
+    }
+    return counter
+}
+
+var result = toTheNthPower(4, 5);
+console.log(result);
+
+
+//fourth function
+function areaOfACircle(radius) {
+    return Math.PI*radius*radius
+    console.log(Math.PI)
+}
+
+areaOfACircle(2);
+
+//fifth function
